919b2d1973811710662040bacbbd1faa16186d8c
IAMR7 <reza.febri72@gmail.com>
2021-06-29 22:22:52 +0700

    update baru

--- assets/js/page/components-chat-box.js
+++ assets/js/page/components-chat-box.js
@@ -87,3 +87,45 @@ $("#chat-form2").submit(function() {
   } 
   return false;
 });
+
+// CHAT PEMBELI
+var chatsPembeli = [
+  {
+    text: 'Halo kak, mau tanya ingkung yang ready yang rasa apa aja ya?',
+    position: 'right'
+  },
+  {
+    text: 'Yang tampil di halaman toko, itu ready semuanya kak',
+    position: 'left'
+  },
+  {
+    text: 'Oooh kok saya gak liat yang rica rica kak',
+    position: 'right'
+  },
+  {
+    text: 'Yang bumbu rica rica nya sedang kosong',
+    position: 'left'
+  },
+];
+for(var i = 0; i < chatsPembeli.length; i++) {
+  var type = 'text';
+  if(chatsPembeli[i].typing != undefined) type = 'typing';
+  $.chatCtrl('#mychatbox3', {
+    text: (chatsPembeli[i].text != undefined ? chatsPembeli[i].text : ''),
+    picture: (chatsPembeli[i].position == 'left' ? 'https://ik.imagekit.io/tvlk/cul-asset/guys1L+Yyer9kzI3sp-pb0CG1j2bhflZGFUZOoIf1YOBAm37kEUOKR41ieUZm7ZJ/tvlk-prod-cul-assets/culinary/asset/REST_000-960x720-FIT_AND_TRIM-cc75c94b625065a494326affcd53ebc5.jpeg?tr=q-40,w-300,h-300&_src=imagekit' : 'assets/img/avatar/avatar-1.png'),
+    position: 'chat-'+chatsPembeli[i].position,
+    type: type
+  });
+}
+$("#chat-form3").submit(function() {
+  var me = $(this);
+
+  if(me.find('input').val().trim().length > 0) {      
+    $.chatCtrl('#mychatbox3', {
+      text: me.find('input').val(),
+      picture: 'http://www.baliutd.com/wp-content/plugins/all-in-one-seo-pack/images/default-user-image.png',
+    });
+    me.find('input').val('');
+  } 
+  return false;
+});
